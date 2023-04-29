29c56d17b3f4536be468ae16937372eca03f22e4
Vanja Ljubobratovic <vanja1708@gmail.com>
2023-04-29 12:09:38 +0200

    Made a script to start bag replay. Modified RUN_ALL.sh with more complex option handling.

--- RUN_ALL.sh
+++ RUN_ALL.sh
@@ -2,6 +2,7 @@
 
 MAPPING=false
 RECORD=false
+PLAY=false
 
 declare -a scripts=("run_camera" "run_imu_filter")
 declare -a titles=("Camera" "Imu_filter")
@@ -12,7 +13,7 @@ do
 
     case $key in
         -h|--help)
-        echo "Usage: $0 [-r|--record] [-m|--mapping] [-h]"
+        echo "Usage: $0 [-r|--record] [-m|--mapping] [-p|--play] [-h]"
         exit 0
         ;;
         -r|--record)
@@ -23,6 +24,10 @@ do
         MAPPING=true
         shift # past argument
         ;;
+        -p|--play)
+        PLAY=true
+        shift # past argument
+        ;;
         *)    # unknown option
         echo "Unknown option: $key"
         exit 1
@@ -30,7 +35,7 @@ do
     esac
 done
 
-#! For readability rather than in loop above
+# For readability rather than in loop above
 if $MAPPING; then
   scripts+=("run_rtabmap")
   titles+=("RTAB_Map")
@@ -41,10 +46,28 @@ if $RECORD; then
   titles+=("Bag_record")
 fi
 
+# Can't run camera when replay is on
+if $PLAY; then
+  scripts+=("replay_bag")
+  unset scripts[0]
+  titles+=("Bag_replay")
+  unset titles[0]
+fi
+
+if $PLAY && $RECORD; then
+  echo "Can't record and play bag at the same time!"
+  exit 1
+fi
+
 # Building command to open all specified scripts in options
 cmd="gnome-terminal"
-for (( i=0; i<${#titles[@]}; i++ )); do
+for (( i=0; i<=${#titles[@]}; i++ )); do
+  if [[ ! -v titles[$i] ]]; then
+    continue
+  fi
   cmd+=" --tab --title=\"${titles[$i]}\" -e \"bash -c 'cd $PWD; ./${scripts[$i]}.sh; $SHELL'\""
 done
+
+echo $cmd
 
 eval "${cmd}"
