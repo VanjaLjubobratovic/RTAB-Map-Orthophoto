2f7e025f52ff9a7ade3f913428c780be577255c3
Vanja Ljubobratović <vanja1708@gmail.com>
2023-05-01 16:43:32 +0200

    Increased rtabmap queue size, set paths to configs, added delete db on start parameter and removed comment

--- run_rtabmap.sh
+++ run_rtabmap.sh
@@ -2,7 +2,8 @@
 source /opt/ros/humble/setup.bash
 
 ros2 launch rtabmap_ros rtabmap.launch.py \
-rviz:=false `#Not needed for now, rtabmapviz suddenly works`\
+rtabmap_args:="--delete_db_on_start" \
+rviz:=false \
 rtabmapviz:=true \
 frame_id:=camera_link \
 rgb_topic:=/camera/color/image_raw \
@@ -10,5 +11,6 @@ depth_topic:=/camera/aligned_depth_to_color/image_raw \
 camera_info_topic:=/camera/color/camera_info \
 approx_sync:=false \
 wait_imu_to_init:=true \
-queue_size:=100 \
-cfg:=/home/vanja/Documents/RTAB-Map/config.ini
+queue_size:=200 \
+cfg:=/home/vanja/RTAB-Map-Orthophoto/rtab_config.ini \
+gui_cfg:=/home/vanja/RTAB-Map-Orthophoto/rtabmapGUI.ini
