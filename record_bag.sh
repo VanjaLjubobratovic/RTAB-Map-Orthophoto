41514faeffd99e9f6c12b78011f2f6016c4b0e4d
Vanja Ljubobratović <vanja1708@gmail.com>
2023-05-02 18:34:57 +0200

    Fixed TF problems with bag recording by recording /tf and /tf_static. Added some tf diagrams for documentation.

--- record_bag.sh
+++ record_bag.sh
@@ -15,9 +15,11 @@ source /opt/ros/humble/setup.bash
 cd ~/camera_bags
 
 ros2 bag record --start-paused -o $filename \
-/tf_static \
 /camera/aligned_depth_to_color/camera_info \
 /camera/aligned_depth_to_color/image_raw \
 /camera/color/camera_info \
 /camera/color/image_raw \
-/camera/imu
+/camera/imu \
+/camera/imu_info \
+/tf_static \
+/tf
