bfce08750bd269570029a029b3efdb01ee6c41d2
Vanja Ljubobratović <vanja1708@gmail.com>
2023-05-06 12:48:28 +0200

    Turned off camera laser. Made new, clean rtabmap config file and started modifying parameters sistematically

--- run_rtabmap.sh
+++ run_rtabmap.sh
@@ -3,8 +3,8 @@ source /opt/ros/humble/setup.bash
 
 ros2 launch rtabmap_ros rtabmap.launch.py \
 rtabmap_args:="--delete_db_on_start" \
-rviz:=true \
-rtabmapviz:=false \
+rviz:=false \
+rtabmapviz:=true \
 frame_id:=camera_link \
 rgb_topic:=/camera/color/image_raw \
 depth_topic:=/camera/aligned_depth_to_color/image_raw \
@@ -12,5 +12,4 @@ camera_info_topic:=/camera/color/camera_info \
 approx_sync:=false \
 wait_imu_to_init:=true \
 queue_size:=300 \
-cfg:=/home/vanja/RTAB-Map-Orthophoto/configs/rtab_config.ini \
-gui_cfg:=/home/vanja/RTAB-Map-Orthophoto/configs/rtabmapGUI.ini
+cfg:=/home/vanja/RTAB-Map-Orthophoto/configs/default_config.ini\
