de8377175d7727d5db55f672bad0647deb705d69
Vanja Ljubobratovic <vanja1708@gmail.com>
2023-10-18 15:08:10 +0200

    Config changes

--- run_camera.sh
+++ run_camera.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-resolution="1280x720x60"
+resolution="848x480x60"
 
 LC_NUMERIC="en_US.UTF-8"; ros2 launch realsense2_camera rs_launch.py \
 rgb_camera.profile:=$resolution \
 depth_module.profile:=$resolution \
 enable_gyro:=true \
 enable_accel:=true \
-unite_imu_method:=1 \
+unite_imu_method:=2 \
 gyro_fps:=400 \
-accel_fps:=250 \
+accel_fps:=200 \
 enable_infra1:=false \
 enable_infra2:=false \
 enable_sync:=false \
@@ -17,6 +17,7 @@ align_depth.enable:=true \
 pointcloud.enable:=false \
 initial_reset:=true \
 depth_module.enable_auto_exposure:=true \
-decimation_filter.enable:=true \
+decimation_filter.enable:=false \
 temporal_filter.enable:=true \
 spatial_filter.enable:=true \
+json_file_path:=/home/vanja/RTAB-Map-Orthophoto/configs/MediumDensity.json
--- run_imu_filter.sh
+++ run_imu_filter.sh
@@ -6,14 +6,15 @@
 
 # 2 options include: Madgwick and Complementary filter
 
-##COMMENT
+#<<COMMENT
 ros2 run imu_complementary_filter complementary_filter_node \
---ros-args --remap /imu/data_raw:=/camera/imu 
-##COMMENT
+--ros-args --remap /imu/data_raw:=/camera/imu \
+--param fixed_frame:=odom
+#COMMENT
 
 <<COMMENT
 ros2 run imu_filter_madgwick imu_filter_madgwick_node \
 --ros-args --remap /imu/data_raw:=/camera/imu \
---param fixed_frame:=camera_link \
---param use_mag:=false
+--param use_mag:=false \
+--param publish_tf:=false
 COMMENT
